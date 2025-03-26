6131153bcfcf37a86534bf98c5b47da2ccca162b
omjaisatya <satyaprakash451256@gmail.com>
2025-03-27 03:24:56 +0530

    modify code for action

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [['module:react-native-dotenv']],
+  presets: ['module:metro-react-native-babel-preset'],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-navigation|react-native|react-native-.*|@react-native-.*)/)',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+  },
 };
