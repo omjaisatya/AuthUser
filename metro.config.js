d4fe10dc85413e1f436434875ce5067964f3c671
omjaisatya <satyaprakash451256@gmail.com>
2025-03-28 05:44:29 +0530

    modify

--- metro.config.js
+++ metro.config.js
@@ -9,3 +9,14 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
